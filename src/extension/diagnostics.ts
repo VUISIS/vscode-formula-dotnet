27111cffea850952331aecf8dae3b413f8b7d207
Stephen Johnson <stephen.johnson@vanderbilt.edu>
2021-11-19 15:00:31 -0600

    Fixed and cleaned up statement ending diagnostics, removed unnecessary dependency.

--- src/extension/diagnostics.ts
+++ src/extension/diagnostics.ts
@@ -5,39 +5,12 @@ export function updateDiagnostics(collection: vscode.DiagnosticCollection) : voi
 	let document = vscode.window.activeTextEditor.document;
 	if (document && path.basename(document.uri.fsPath).endsWith('.4ml')) {
 		let cols = [];
-        let cCC = checkConstructorCase(document);
-        if(cCC.length > 0)
+        let cGC = checkGenericCase(document);
+        if(cGC.length > 0)
         {
-            for(let i = 0;i < cCC.length;++i)
+            for(let i = 0;i < cGC.length;++i)
             {
-                cols.push(cCC[i]);
-            }
-        }
-           
-        let cFE = checkForExpression(document);
-        if(cFE.length > 0)
-        {
-            for(let i = 0;i < cFE.length;++i)
-            {
-                cols.push(cFE[i]);
-            }
-        }
-
-        let cSE = checkForSubExpression(document);
-        if(cSE.length > 0)
-        {
-            for(let i = 0;i < cSE.length;++i)
-            {
-                cols.push(cSE[i]);
-            }
-        }
-
-        let cCE = checkConformsExpression(document);
-        if(cCE.length > 0)
-        {
-            for(let i = 0;i < cCE.length;++i)
-            {
-                cols.push(cCE[i]);
+                cols.push(cGC[i]);
             }
         }
 
@@ -47,219 +20,64 @@ export function updateDiagnostics(collection: vscode.DiagnosticCollection) : voi
 	}
 }
 
-function checkConstructorCase(document: vscode.TextDocument) : Array<{}>
+function checkGenericCase(document: vscode.TextDocument) : Array<{}>
 {
     let cols = [];
-    let re = new RegExp('::=', 'g');
-    let match;
-    let txt = document.getText();
-    while ((match = re.exec(txt)) != null) 
+    for(let i = 0;i < document.lineCount;++i)
     {
-        let pos = document.positionAt(match.index);
-        let line = document.lineAt(pos.line);
-        let whitespaceRemoved = line.text.replace(/\s/g, "");
-        let dotReg = new RegExp('[\)]$', 'g');;
-        let endOfLine = dotReg.exec(whitespaceRemoved);
-        let check = checkForSingleLineComments(whitespaceRemoved);
-        if(endOfLine === null || 
-           endOfLine.length <= 0 ||
-           whitespaceRemoved.endsWith(".") ||
-           check)
+        let line = document.lineAt(i);
+        let match =/([\)}]$)|([a-zA-Z0-9]$)/g.test(line.text);
+        if(match)
         {
-            continue;
-        }
-        else
-        {
-            cols.push({
-                code: '',
-                message: "Constructor statements end with a '.'",
-                range: new vscode.Range(line.range.end, line.range.end.translate(0,1)),
-                severity: vscode.DiagnosticSeverity.Error
-            });
+            let whitespaceRemoved = line.text.replace(/\s/g, "");
+            let isComment = checkForSingleLineComments(whitespaceRemoved);
+            let isClosingBracket = checkForClosingBracket(whitespaceRemoved);
+            let isDefinition = false;
+            if((i+1) < document.lineCount)
+            {
+                let nextLine = document.lineAt(i+1);
+                isDefinition = checkForDefinition(line, nextLine);
+            }
+            if(isDefinition ||
+               isClosingBracket ||
+               isComment)
+            {
+                continue;
+            }
+            else
+            {
+                cols.push({
+                    code: '',
+                    message: "Statements end in a '.' or ','",
+                    range: new vscode.Range(line.range.end, line.range.end.translate(0,1)),
+                    severity: vscode.DiagnosticSeverity.Error
+                });
+            }
         }
     }
     return cols;
-} 
+}
+
+function checkForDefinition(line: vscode.TextLine, nextLine: vscode.TextLine) : boolean
+{
+    let re = /(requires|ensures|initially|next|domain|model|transform|returns|machine)/g;
+    let re2 = /({|returns)/g;
+    console.log(nextLine.text);
+    return re.test(line.text) && re2.test(nextLine.text);
+}
 
 function checkForSingleLineComments(text: string) : boolean
 {
-    let re = new RegExp('^\/+', 'g');
+    let re = /(^ *)(\/.*)/g;
     return re.test(text);
 }
 
-function checkForExpression(document: vscode.TextDocument) : Array<{}>
+function checkForClosingBracket(text: string) : boolean
 {
-    let cols = [];
-    let re = new RegExp('[\+]', 'g');
-    let match;
-    let txt = document.getText();
-    while ((match = re.exec(txt)) != null) 
+    if(text.includes("}") &&
+       text.length < 2)
     {
-        let pos = document.positionAt(match.index);
-        let line = document.lineAt(pos.line);
-        let whitespaceRemoved = line.text.replace(/\s/g, "");
-        let dotReg = new RegExp('[a-z]$', 'g');;
-        let endOfLine = dotReg.exec(whitespaceRemoved);
-        let check = checkForSingleLineComments(whitespaceRemoved);
-        if(endOfLine === null || 
-           endOfLine.length <= 0 ||
-           whitespaceRemoved.endsWith(".") ||
-           check)
-        {
-            continue;
-        }
-        else
-        {
-            cols.push({
-                code: '',
-                message: "Expression statements end with a '.'",
-                range: new vscode.Range(line.range.end, line.range.end.translate(0,1)),
-                severity: vscode.DiagnosticSeverity.Error
-            });
-        }
+        return true;
     }
-    return cols;
-}
-
-
-function checkForSubExpression(document: vscode.TextDocument) : Array<{}>
-{
-    let cols = [];
-    let re = new RegExp('(::=)', 'g');
-    let match;
-    let txt = document.getText();
-    while ((match = re.exec(txt)) != null) 
-    {
-        let pos = document.positionAt(match.index);
-        let line = document.lineAt(pos.line);
-        let whitespaceRemoved = line.text.replace(/\s/g, "");
-        let check = checkForSingleLineComments(whitespaceRemoved);
-        if(whitespaceRemoved.endsWith(".") ||
-           check)
-        {
-            continue;
-        }
-        else if(whitespaceRemoved.endsWith(","))
-        {
-            let flag = true;
-            let nextPos = pos.translate(1,0);
-            let nextLine = document.lineAt(nextPos.line);
-            let wsRemoved = nextLine.text.replace(/\s/g, "");
-            while(flag)
-            {        
-                if(wsRemoved.endsWith(","))
-                {
-                    nextPos = nextPos.translate(1,0);
-                    nextLine = document.lineAt(nextPos.line);
-                    wsRemoved = nextLine.text.replace(/\s/g, "");
-                }
-                else if (wsRemoved.endsWith(".") || wsRemoved.endsWith(")"))
-                {
-                    flag = false;
-                }
-            }
-
-            if(wsRemoved.endsWith("."))
-            {
-                continue;
-            }
-            else
-            {
-                cols.push({
-                    code: '',
-                    message: "Sub expression statements end with a '.'",
-                    range: new vscode.Range(nextLine.range.end, nextLine.range.end.translate(0,1)),
-                    severity: vscode.DiagnosticSeverity.Error
-                });
-            }
-        }
-    }
-    return cols;
-}
-
-function checkConformsExpression(document: vscode.TextDocument) : Array<{}>
-{
-    let cols = [];
-    let re = new RegExp('(conforms)', 'g');
-    let match;
-    let txt = document.getText();
-    while ((match = re.exec(txt)) != null) 
-    {
-        let pos = document.positionAt(match.index);
-        let line = document.lineAt(pos.line);
-        let whitespaceRemoved = line.text.replace(/\s/g, "");
-        let dotReg = new RegExp('[\|]$', 'g');
-        let endOfLine = dotReg.test(whitespaceRemoved);
-        let check = checkForSingleLineComments(whitespaceRemoved);
-        let checkAfterKeyword = new RegExp('(conforms)$', 'g');
-        if(whitespaceRemoved.endsWith(".") ||
-           check)
-        {
-            continue;
-        }
-        else if(checkAfterKeyword.test(whitespaceRemoved))
-        {
-            cols.push({
-                code: '',
-                message: "Conform expressions end with a '.'",
-                range: new vscode.Range(line.range.end, line.range.end.translate(0,1)),
-                severity: vscode.DiagnosticSeverity.Error
-            });
-            continue;
-        }
-        else if(endOfLine)
-        {
-            let flag = true;
-            let nextPos = pos.translate(1,0);
-            let nextLine = document.lineAt(nextPos.line);
-            let wsRemoved = nextLine.text.replace(/\s/g, "");
-            if(checkForSingleLineComments(wsRemoved))
-            {
-                flag = false;
-            }
-            let endOfRec = new RegExp('[\|]','g');
-            let endOfExpr = new RegExp('[}]$','g');
-            while(flag)
-            {        
-                if(endOfRec.test(wsRemoved))
-                {
-                    nextPos = nextPos.translate(1,0);
-                    nextLine = document.lineAt(nextPos.line);
-                    wsRemoved = nextLine.text.replace(/\s/g, "");
-                    if(checkForSingleLineComments(wsRemoved))
-                    {
-                        flag = false;
-                    }
-                }
-                else if (wsRemoved.endsWith(".") || endOfExpr.test(wsRemoved))
-                {
-                    flag = false;
-                }
-            }
-
-            if(wsRemoved.endsWith(".") || checkForSingleLineComments(wsRemoved))
-            {
-                continue;
-            }
-            else
-            {
-                cols.push({
-                    code: '',
-                    message: "Conform expressions end with a '.'",
-                    range: new vscode.Range(nextLine.range.end, nextLine.range.end),
-                    severity: vscode.DiagnosticSeverity.Error
-                });
-            }
-        }
-        else if(whitespaceRemoved.endsWith("}") || whitespaceRemoved.endsWith(")"))
-        {
-            cols.push({
-                code: '',
-                message: "Conform expressions end with a '.'",
-                range: new vscode.Range(line.range.end, line.range.end),
-                severity: vscode.DiagnosticSeverity.Error
-            });
-        }
-    }
-    return cols;
+    return false;
 }
